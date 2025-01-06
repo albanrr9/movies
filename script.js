23dd4f73b16f287dccce6e1cf37f07f772ff8d76
Alban Rrahmani <albanrr9@gmail.com>
2025-01-06 23:23:49 +0100

    1

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const twoembed = document.getElementById("2embed");
 const superembed = document.getElementById("superembed");
 
+function generateEmbedUrls(searchInput, type, isId) {
+  const baseSuperembedUrl = "https://multiembed.mov/directstream.php?video_id=";
+  const baseTwoembedUrl = "https://www.2embed.cc/";
+
+  if (type === "movie") {
+    return {
+      superembedUrl: `${baseSuperembedUrl}${searchInput}${isId ? "" : "&tmdb=1"}`,
+      twoembedUrl: `${baseTwoembedUrl}embed/${searchInput}`
+    };
+  } else if (type === "tvshow") {
+    return {
+      superembedUrl: `${baseSuperembedUrl}${searchInput}&s=1&e=1${isId ? "" : "&tmdb=1"}`,
+      twoembedUrl: `${baseTwoembedUrl}embedtv/${searchInput}&s=1&e=1`
+    };
+  }
+
+  return { superembedUrl: "", twoembedUrl: "" };
+}
+
 function handleSearch() {
   const selectedRadio = document.querySelector('input[name="watchtype"]:checked');
   const searchInput = document.getElementById('searchInput').value.trim().toLowerCase();
 
-  if (!selectedRadio) { // If no radio button is selected
+  if (!selectedRadio) {
     alert('Please select a search type (Movie or TV Show)!');
-    return; // Stop further execution
+    return;
   }
 
   if (!searchInput) {
@@ -15,28 +34,16 @@ function handleSearch() {
     return;
   }
 
-  let id = searchInput.startsWith("tt");
+  const isId = searchInput.startsWith("tt");
+  const { superembedUrl, twoembedUrl } = generateEmbedUrls(searchInput, selectedRadio.value, isId);
 
-  if (selectedRadio.value === "movie") {
-    if (id == true) {
-      var superembedurl = "https://multiembed.mov/directstream.php?video_id=" + searchInput;
-      var twoembedurl = "https://www.2embed.cc/embed/" + searchInput;
-    }
-    else {
-      var superembedurl = "https://multiembed.mov/directstream.php?video_id=" + searchInput + "&tmdb=1";
-      var twoembedurl = "https://www.2embed.cc/embed/" + searchInput;
-    }
-  }
-  else if (selectedRadio.value === "tvshow") {
-    if (id == true) {
-      var superembedurl = "https://multiembed.mov/directstream.php?video_id=" + searchInput + "&s=1&e=1";
-      var twoembedurl = "https://www.2embed.cc/embedtv/" + searchInput + "&s=1&e=1";
-    }
-    else {
-      var superembedurl = "https://multiembed.mov/directstream.php?video_id=" + searchInput + "&tmdb=1&s=1&e=1";
-      var twoembedurl = "https://www.2embed.cc/embedtv/" + searchInput + "&s=1&e=1";
-    }
-  }
-  superembed.innerHTML = `<iframe src="${superembedurl}" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>`
-  twoembed.innerHTML = `<iframe src="${twoembedurl}" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>`
+  // Adding loading indicator
+  superembed.innerHTML = "<p>Loading...</p>";
+  twoembed.innerHTML = "<p>Loading...</p>";
+
+  // Embedding the iframes
+  setTimeout(() => {
+    superembed.innerHTML = `<iframe src="${superembedUrl}" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+    twoembed.innerHTML = `<iframe src="${twoembedUrl}" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>`;
+  }, 1000); // Simulate loading delay
 }
